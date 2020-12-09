b4aa738fce4cd8d5a3323a108e0548c02690eb02
verelarinzanoer <68377120+verelarinzanoer@users.noreply.github.com>
2020-12-09 10:20:42 +0700

    Add files via upload

---
+++ wecare orphans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wecare orphans
+//
+//  Created by MACBOOK on 09/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
